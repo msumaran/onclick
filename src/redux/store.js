48450a9d22369bfe4c563699917fb92d0fc10b71
Carlos Aguinaga <andres.aguinaga@gmail.com>
2021-02-19 16:00:17 -0500

    add dev logs to redux store changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,30 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from 'redux/reducers'
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const logger = (store) => (next) => (action) => {
+
+    let result = next(action)
+
+    if (process.env.REACT_APP_ENVIRONMENT !== 'dev'){
+
+        return result
+    }
+
+    if (typeof action === 'function') return result
+
+    console.group('State modified')
+
+    console.log('%cType: %c' + action.type, 'font-weight: bold;', 'color: red')
+
+    if (action.content) console.log('%cContent', 'font-weight: bold;', action.content)
+
+    console.log('%cNew state', 'font-weight: bold;', store.getState())
+
+    console.groupEnd()
+
+    return result
+}
+
+const store = createStore(reducers, applyMiddleware(logger, thunk))
 
 export default store
