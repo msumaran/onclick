9621ea665c70a658a69064069483fea9a34e450b
Carlos Aguinaga <andres.aguinaga@gmail.com>
2021-05-03 10:45:16 -0500

    add feedback modal

--- src/views/Feedback/Feedback.js
+++ src/views/Feedback/Feedback.js
@@ -1,8 +1,8 @@
 /*eslint no-unused-vars: "off" */
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Card, CardBody, CardHeader, Col, Row } from 'reactstrap'
+import { Card, CardBody, CardHeader, Col, Row, Form, FormGroup, Label, Modal, ModalBody, ModalHeader } from 'reactstrap'
 
 import PermissionHelper from 'helpers/PermissionHelper'
 import { StripedTable } from 'components/CustomTable'
@@ -18,6 +18,27 @@ const Feedback = () => {
 
     const feedback_result = useSelector(state => state.FeedbackReducer.result)
     const feedback_load_status = useSelector(state => state.FeedbackReducer.load_status)
+
+    const [ showModal, setShowModal ] = useState(false)
+    const [ feedbackRow, setFeedbackRow ] = useState({
+        user: {}
+    })
+
+    const selectRow = (row) => {
+
+        setFeedbackRow(row)
+
+        setShowModal(true)
+    }
+
+    const getRowType = (row) => {
+
+        switch (row.type) {
+            case 'sugerencia': return 'Sugerencia'
+            case 'problema': return 'Queja o problema'
+            default: return ''
+        }
+    }
 
     useEffect(() => {
 
@@ -40,19 +61,13 @@ const Feedback = () => {
                                 {!permission_helper.validate('feedback', 'r') ? null : (
                                     <StripedTable
                                         columns={[
-                                            { Header: 'Tipo', accessor: 'type', Cell: ({ row: { original } }) => {
-
-                                                console.log({ original })
-
-                                                switch (original.type) {
-                                                    case 'sugerencia': return 'Sugerencia'
-                                                    case 'problema': return 'Queja o problema'
-                                                    default: return ''
-                                                }
-                                            } },
+                                            { Header: 'Tipo', accessor: 'type', Cell: ({ row: { original } }) => getRowType(original) },
                                             { Header: 'Usuario', accessor: 'user.name' },
                                             { Header: 'Acciones', width: 250, Cell: ({ row: { original } }) => (
                                                 <>
+                                                    <button className="btn btn-secondary" onClick={() => selectRow(original)}>
+                                                        Leer
+                                                    </button>
                                                 </>
                                             )}
                                         ]}
@@ -68,6 +83,61 @@ const Feedback = () => {
                     </Card>
                 </Col>
             </Row>
+
+            <Modal size="lg" isOpen={showModal} toggle={() => setShowModal(!showModal)}>
+                <ModalHeader toggle={() => setShowModal(false)}>Mensaje de feedback</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup row>
+                            <Label sm={3}>Fecha de mensaje</Label>
+                            <Col sm={9}>
+                                <input
+                                    type="text"
+                                    className="form-control-plaintext"
+                                    defaultValue={feedbackRow.createdAt}
+                                    readOnly
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label sm={3}>Usuario</Label>
+                            <Col sm={9}>
+                                <input
+                                    type="text"
+                                    className="form-control-plaintext"
+                                    defaultValue={feedbackRow.user.name}
+                                    readOnly
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label sm={3}>Tipo de feedback</Label>
+                            <Col sm={9}>
+                                <input
+                                    type="text"
+                                    className="form-control-plaintext"
+                                    defaultValue={getRowType(feedbackRow)}
+                                    readOnly
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label sm={3}>Mensaje</Label>
+                            <Col sm={9}>
+                                <textarea
+                                    type="text"
+                                    className="form-control-plaintext"
+                                    defaultValue={feedbackRow.message}
+                                    readOnly
+                                    style={{
+                                        resize: 'none'
+                                    }}
+                                ></textarea>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+            </Modal>
         </div>
     )
 }
