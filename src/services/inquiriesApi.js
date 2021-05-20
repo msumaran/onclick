fd54080b9a6287c78722f63e56539dba9cb1af0d
Carlos Aguinaga <andres.aguinaga@gmail.com>
2021-05-20 17:51:57 -0500

    add filter to messages

--- src/services/inquiriesApi.js
+++ src/services/inquiriesApi.js
@@ -3,11 +3,11 @@ import apiService from './apiService'
 
 const api_base = '/inquiry'
 
-export const findAll = async () => {
+export const findAll = async (filter = '') => {
 
     try {
 
-        return await apiService.get(`${api_base}/combobox`)
+        return await apiService.get(`${api_base}/combobox?filter=${filter}`)
     } catch (error) {
 
         throw error
